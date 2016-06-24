ed7b69d926546f39bc86f591eb6e3c4649f30414
shinespark <shinespark64@gmail.com>
2016-06-24 16:15:18 -0700

    add gulp-ruby-haml

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var HTML_SRC = './assets/views/**/*.html';
-var SCSS_SRC = './assets/stylesheets/**/*.scss';
-var CSS_SRC  = './css/**/*.css';
-var CSS_DEST = './css/';
+var HAML_SRC  = './assets/views/**/*.haml';
+var HTML_SRC  = './**/*.html';
+var HTML_DEST = './';
+var SCSS_SRC  = './assets/stylesheets/**/*.scss';
+var CSS_SRC   = './css/**/*.css';
+var CSS_DEST  = './css/';
 
 var gulp = require('gulp');
+var haml = require('gulp-ruby-haml');
 var sass = require('gulp-sass');
 var scsslint = require('gulp-scss-lint');
 var bs = require('browser-sync').create();
@@ -19,6 +22,22 @@ gulp.task('bs', function(){
   bsOptions.proxy = '192.168.99.100:3000';
   bsOptions.port  = 3001;
   bs.init(bsOptions);
+});
+
+
+// haml
+gulp.task('haml', function(){
+  return gulp.src(HAML_SRC)
+    .pipe(haml().on('error', function(e) { console.log(e.message); }))
+    .pipe(gulp.dest(HTML_DEST));
+});
+
+// haml-watch
+gulp.task('haml:watch', function(){
+  var watcher = gulp.watch(HAML_SRC, ['haml']);
+  watcher.on('change', function(event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+  });
 });
 
 
